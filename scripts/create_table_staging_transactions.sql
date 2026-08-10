7dfa33928be73c044bcc51eff06a281a1e81734b
izsnr <izsnr@szlaptop.lan>
2026-08-09 21:35:59 -0500

    working kafka > bento > postgresql pipeline with dlq

---
+++ scripts/create_table_staging_transactions.sql
@@ -0,0 +1,19 @@
+SET search_path TO staging; -- You can alternatively do CREATE TABLE staging.transactions, but I like this more
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    alert_datetime TIMESTAMPTZ NOT NULL,
+    bank VARCHAR(50) NOT NULL,                                            -- Entity issuing the card/alert (e.g. 'Citi')
+    vendor VARCHAR(255) NOT NULL,                                         -- Name of Merchant/Vendor where money was spent
+    amount NUMERIC(10, 2) NOT NULL CHECK (amount >= 0),                    -- Monetary amount
+    transaction_type VARCHAR(20) NOT NULL DEFAULT 'PURCHASE' CHECK (transaction_type IN ('PURCHASE', 'CREDIT')),
+    card_last_4 CHAR(4) NOT NULL,                                         -- String to preserve leading zeros "e.g. ending in 0123"
+    is_card_not_present BOOLEAN NOT NULL DEFAULT FALSE,                   -- Only true when detecting "Card not present"
+    raw_text TEXT NOT NULL,                                               -- Unparsed data from Redpanda Bronze Layer
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uq_transactions_dedup UNIQUE (bank, card_last_4, alert_datetime, amount) -- Postgres requires this, otherwise you get the error: there is no unique or exclusion constraint matching the ON CONFLICT specification
+);
+      
+CREATE INDEX IF NOT EXISTS idx_transactions_alert_datetime ON transactions (alert_datetime DESC);
+CREATE INDEX IF NOT EXISTS idx_transactions_bank ON transactions (bank);
+CREATE INDEX IF NOT EXISTS idx_transactions_vendor ON transactions (vendor);
---
+++ scripts/create_table_staging_transactions_dlq.sql
@@ -0,0 +1,9 @@
+CREATE TABLE staging.transactions_dlq (
+    dlq_id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    raw_text         TEXT NOT NULL,
+    error_reason     TEXT NOT NULL,
+    error_detail     TEXT,
+    kafka_partition  INT,
+    kafka_offset     BIGINT,
+    created_at       TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
